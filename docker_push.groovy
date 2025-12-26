44f4a7ab1d0fd2a06529cc072c5e7096b5e4efa9
Awnish prasad sharma <awnishprasad99@gmail.com>
2025-12-26 17:36:10 +0530

    Add Docker login and push steps to docker_push.groovy
    Implement Docker login and image push in pipeline.

---
+++ docker_push.groovy
@@ -0,0 +1,9 @@
+def call(String credId, String imageName){
+      withCredentials([usernamePassword(
+                    credentialsId:"dockerHubCreds",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")]){
+                sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
+                sh "docker image tag node-app:latest ${env.dockerHubUser}/node-app:latest"
+                sh "docker push ${env.dockerHubUser}/node-app:latest"
+}
